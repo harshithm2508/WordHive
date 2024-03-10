4dc20e59b2ae82dcd78838201171ac89efbdd773
harshithm2508 <harshithm2508@gmail.com>
2024-03-10 18:06:54 +0000

    added signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,48 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+import { use } from 'hono/jsx';
 
 const app = new Hono<{
-	Bindings: {
-		DATABASE_URL: string
-	}
+  Bindings: {
+    JWT_SECRET: string;
+    DATABASE_URL: string
+  }
 }>();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/user/signup',(c)=>{
+app.post('/api/v1/user/signup',async (c)=>{
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-  return c.text("User is signing up")
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  try{
+    const user = await prisma.user.create({
+      data : {
+        email : body.email,
+        password : body.password,
+        name : body.name
+      }
+    })
+    
+    const token = await sign({
+      id : user.id
+    },c.env.JWT_SECRET)
+
+    console.log(token)
+
+    return c.text("Signed Up")
+  }catch(e){
+    return c.json({
+      error : e
+    })
+  }
 })
 
 app.post('/api/v1/user/signin',(c)=>{
