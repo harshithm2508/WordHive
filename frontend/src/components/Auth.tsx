88d79535e386a7b3e54af21733b7ee4e1db2dd7b
harshithm2508 <harshithm2508@gmail.com>
2024-03-30 17:01:51 +0000

    set password invisibility(type a  prop)

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -10,7 +10,6 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
     })
     return(
         <div className="h-screen flex flex-col justify-center items-center">
-            {JSON.stringify(postInputs)}
             <div className="w-2/4">
                 <div className="text-3xl text-center font-extrabold pb-2">
                     {type === 'signin' ? "Login to your account" : "Create an account"}
@@ -32,7 +31,7 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
                     })
                 }}/>
 
-                <LabelledInput label="Password" placeholder="Enter your password" onChange={(e)=>{
+                <LabelledInput label="Password" placeholder="Enter your password" type="password" onChange={(e)=>{
                     setPostInputs({
                         ...postInputs,
                         password : e.target.value
@@ -51,14 +50,15 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
 interface LabelledInputType{
     label : string;
     placeholder : string,
-    onChange : (e: ChangeEvent<HTMLInputElement>)=>void
+    onChange : (e: ChangeEvent<HTMLInputElement>)=>void,
+    type? : string
 }
 
-function LabelledInput({label, placeholder, onChange} : LabelledInputType){
+function LabelledInput({label, placeholder,type, onChange} : LabelledInputType){
     return(
         <div className="my-5">
             <label  className="block mb-2 text-sm font-bold text-gray-900 pt-2 dark:text-black">{label}</label>
-            <input type="text" id="first_name" className="bg-gray-50 shadow-md	border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onChange={onChange} placeholder={placeholder} required />
+            <input type={type || 'text'} id="first_name" className="bg-gray-50 shadow-md	border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onChange={onChange} placeholder={placeholder} required />
         </div>
     )
 }
