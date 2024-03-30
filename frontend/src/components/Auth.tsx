2447c672d70b65acf9a75e3b74d9eb131e94f5ef
harshithm2508 <harshithm2508@gmail.com>
2024-03-30 14:27:35 +0000

    SignIn SignUp page ready

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useState} from "react"
 import { Link } from "react-router-dom"
 import { SignupType, signupInput } from "@harshithm2508/wordhivecommon"
 
-export const Auth = ()=>{
+export const Auth = ({type} : {type : "signup" | "signin"})=>{
     const [postInputs, setPostInputs] = useState<SignupType>({
         email : "",
         password : "",
@@ -10,32 +10,39 @@ export const Auth = ()=>{
     })
     return(
         <div className="h-screen flex flex-col justify-center items-center">
-            <div className="text-3xl font-extrabold pb-2">
-                Create an Account
-            </div>
-            <div>
-                Already have an account? <Link className="pl-2 underline" to={'/signin'}>Login</Link>
-            </div>
-            <LabelledInput label="Username" placeholder="Enter your username" onChange={(e)=>{
-                setPostInputs({
-                    ...postInputs,
-                    name : e.target.value
-                })
-            }}/>
+            <div className="w-2/4">
+                <div className="text-3xl text-center font-extrabold pb-2">
+                    {type === 'signin' ? "Login to your account" : "Create an account"}
+                </div>
+                <div className="text-center">
+                    {type==="signin" ? "Don't have an account?" : "Already have an account?"} <Link className="pl-2 underline" to={type === 'signin' ? '/signup' : '/signin'}>{type==="signin" ? "Sign Up" : "Login"}</Link>
+                </div>
+                <LabelledInput label="Name" placeholder="Enter your Name" onChange={(e)=>{
+                    setPostInputs({
+                        ...postInputs,
+                        name : e.target.value
+                    })
+                }}/>
 
-            <LabelledInput label="Password" placeholder="Enter your username" onChange={(e)=>{
-                setPostInputs({
-                    ...postInputs,
-                    name : e.target.value
-                })
-            }}/>
+                <LabelledInput label="E-mail" placeholder="Enter your E-mail" onChange={(e)=>{
+                    setPostInputs({
+                        ...postInputs,
+                        name : e.target.value
+                    })
+                }}/>
 
-            <LabelledInput label="Password" placeholder="Enter your username" onChange={(e)=>{
-                setPostInputs({
-                    ...postInputs,
-                    name : e.target.value
-                })
-            }}/>
+                <LabelledInput label="Password" placeholder="Enter your password" onChange={(e)=>{
+                    setPostInputs({
+                        ...postInputs,
+                        name : e.target.value
+                    })
+                }}/>
+
+                {/* //Sign In (or) SignUp button */}
+                <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
+                focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 
+                dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signin" ? "Sign In" : "Sign Up"}</button>
+            </div>
         </div>
     )
 }
@@ -48,9 +55,9 @@ interface LabelledInputType{
 
 function LabelledInput({label, placeholder, onChange} : LabelledInputType){
     return(
-        <div>
-            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">{label}</label>
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+        <div className="my-5">
+            <label  className="block mb-2 text-sm font-bold text-gray-900 pt-2 dark:text-black">{label}</label>
+            <input type="text" id="first_name" className="bg-gray-50 shadow-md	border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
         </div>
     )
 }
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,7 +1,17 @@
+import { Quote } from "../components/Quote"
+import { Auth } from "../components/Auth"
+
 export const SignUp = () => {
     return(
         <>
-            SignUp
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className="">
+                    <Auth type="signup"/>
+                </div>
+                <div className="hidden lg:block">
+                    <Quote/>
+                </div>
+            </div>
         </>
     )
 }
