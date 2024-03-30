1a768a5670a0725c65ccdfaa1ddcc2913a97b860
harshithm2508 <harshithm2508@gmail.com>
2024-03-30 16:58:39 +0000

    name email password updating

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -10,6 +10,7 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
     })
     return(
         <div className="h-screen flex flex-col justify-center items-center">
+            {JSON.stringify(postInputs)}
             <div className="w-2/4">
                 <div className="text-3xl text-center font-extrabold pb-2">
                     {type === 'signin' ? "Login to your account" : "Create an account"}
@@ -27,14 +28,14 @@ export const Auth = ({type} : {type : "signup" | "signin"})=>{
                 <LabelledInput label="E-mail" placeholder="Enter your E-mail" onChange={(e)=>{
                     setPostInputs({
                         ...postInputs,
-                        name : e.target.value
+                        email : e.target.value
                     })
                 }}/>
 
                 <LabelledInput label="Password" placeholder="Enter your password" onChange={(e)=>{
                     setPostInputs({
                         ...postInputs,
-                        name : e.target.value
+                        password : e.target.value
                     })
                 }}/>
 
@@ -57,7 +58,7 @@ function LabelledInput({label, placeholder, onChange} : LabelledInputType){
     return(
         <div className="my-5">
             <label  className="block mb-2 text-sm font-bold text-gray-900 pt-2 dark:text-black">{label}</label>
-            <input type="text" id="first_name" className="bg-gray-50 shadow-md	border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+            <input type="text" id="first_name" className="bg-gray-50 shadow-md	border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onChange={onChange} placeholder={placeholder} required />
         </div>
     )
 }
