1d78d09e7a8a3b4af16f308b6c4670b2d5c1f2dd
harshithm2508 <harshithm2508@gmail.com>
2024-03-31 17:33:32 +0000

    set cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
 import { use } from 'hono/jsx';
+import { cors } from 'hono/cors'
 import userRouter from '../routes/user';
 import blogRouter from '../routes/blog';
 
@@ -14,6 +15,7 @@ const app = new Hono<{
 }>();
 
 
+app.use('/*', cors())
 app.route('/api/v1/user',userRouter);
 app.route('/api/v1/blog',blogRouter);
 
