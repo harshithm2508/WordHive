e23dac0e89e2d1776b2fd667ae583d504d721a30
Harshith Muthangi <harshithm2508@gmail.com>
2024-08-22 16:08:15 +0530

    added jwt for signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import { decode,sign, verify } from 'hono/jwt'
 
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET : string
   }
 }>()
 
@@ -24,15 +25,19 @@ app.post('/api/v1/user/signup',async (c) => {
   }).$extends(withAccelerate())
 
   try{
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data : {
         username : body.username,
         password : body.password,
         name : body.name
       }
     })
+
+    const jwt = await sign({
+      id : user.id
+    },c.env.JWT_SECRET)
   
-    return c.text("User created")
+    return c.text(jwt)
   }catch(e){
     c.status(411);
     return c.text("Invalid")
@@ -60,3 +65,6 @@ app.get('api/v1/blog/bulk', (c)=>{
 })
 
 export default app;
+
+
+// jwt for harshith : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NH0.AUIEs1lI8DEQoPeHmLtaAB1H7fSq_7ytG_6ndHPNTvM
