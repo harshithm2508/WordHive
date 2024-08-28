00d0b8fe16933b776358b2b9729c697e3340b156
harshithm2508 <harshithm2508@gmail.com>
2024-08-28 16:17:50 +0530

    added db and pool

--- backend/src/routes/blogs.ts
+++ backend/src/routes/blogs.ts
@@ -17,3 +17,7 @@ blogRouter.get('/bulk', (c)=>{
 blogRouter.get('/:id',(c) => {
     return c.text("User gets his blog at this endpoint.")
 })
+
+
+//  DATABASE_URL : postgresql://wordhive-db_owner:dl5kPYm3sMCQ@ep-crimson-rice-a5e26ahn.us-east-2.aws.neon.tech/wordhive-db?sslmode=require
+//  Pool lin  :     DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiN2NiMzYxOWYtMGNmNS00NWY5LTg3Y2EtODM5ODg5YWY3MTcwIiwidGVuYW50X2lkIjoiYWYzN2EwN2M2NjQyYzZlMjQ4ZjU1NzE2ZjNkMzJkZTRmMWU4M2Y4NTBlMjQzNjUzYzZkMWE5YjQ3YWMxZTY0YyIsImludGVybmFsX3NlY3JldCI6ImE0OTc3OTM0LWMwYjktNGEyOS1iOWYzLWM3ZGI1MjZjNDQ5MSJ9.nR5zx3xc8zBrS9H-TlEtj4sx_ayhvs4N-HPYWxV2xTU"
