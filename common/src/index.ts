a10062a9a8bd0a0e14f1190aecc112587bb0c2ca
harshithm2508 <harshithm2508@gmail.com>
2024-09-01 16:06:57 +0530

    updated zod types

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,34 +1,33 @@
-import z from "zod";
+import z, { string } from 'zod';
 
 export const signupInput = z.object({
-    username: z.string().email(),
-    password: z.string(),
-    name: z.string().optional(),
-});
+    username : z.string().email(),
+    password : z.string().min(6),
+    name : z.string().optional()
+})
+
 
 
 export const signinInput = z.object({
-    username: z.string().email(),
-    password: z.string(),
-});
+    username : z.string().email(),
+    password : z.string().min(6)
+})
 
 
-export const createPostInput = z.object({
-    title: z.string(),
-    content: z.string(),
-});
+export const createBlogInput = z.object({
+    title : z.string(),
+    content : z.string()
+})
 
 
-export const updatePostInput = z.object({
-    title: z.string().optional(),
-    content: z.string().optional(),
-});
+export const updateBlogInput = z.object({
+    title : z.string(),
+    content : z.string(),
+    id : z.number()
+})
 
 
-
-
-
-export type SignupType = z.infer<typeof signupInput>;
-export type SigninType = z.infer<typeof signinInput>;
-export type CreatePostType = z.infer<typeof createPostInput>;
-export type UpdatePostType = z.infer<typeof updatePostInput>;
+export type SignUpInput = z.infer<typeof signupInput>
+export type SignInInput = z.infer<typeof signinInput>
+export type CreateBlogInput = z.infer<typeof createBlogInput>
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>
