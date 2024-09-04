1b7ad59361354aa277d97cc07b6256bd08013f64
harshithm2508 <harshithm2508@gmail.com>
2024-09-04 12:51:18 +0530

    backend blog route update

--- backend/src/routes/blogs.ts
+++ backend/src/routes/blogs.ts
@@ -150,6 +150,15 @@ blogRouter.get('/:id',async (c) => {
         const blog = await prisma.blog.findFirst({
             where : {
                 id : Number(id)
+            },
+            select : {
+                title : true,
+                content : true,
+                author : {
+                    select : {
+                        name : true
+                    }
+                }
             }
         })
 
