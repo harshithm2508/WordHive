8546017163e180f27b221582603619a166bd64f0
harshithm2508 <harshithm2508@gmail.com>
2024-09-05 16:44:21 +0530

    successful blog publish

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -2,14 +2,21 @@ import axios from "axios"
 import Appbar from "../components/Appbar"
 import { BACKEND_URL } from "../config"
 import { ChangeEvent, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const Publish = () =>{
 
     const [ title, setTitle ] = useState('');
     const [ content, setContent ] = useState('');
+    const navigate = useNavigate();
 
-    function publishBlog(){
-        axios.post(`${BACKEND_URL}api/v1/blog`)
+    async function publishBlog(){
+        const response = await axios.post(`${BACKEND_URL}api/v1/blog`, {
+            title, content
+        }, {headers : {
+            Authorization : localStorage.getItem('token')
+        }})
+        navigate(`/blog/${response.data.id}`)
     }
 
     return(
@@ -28,9 +35,6 @@ export const Publish = () =>{
                     <div className=" mt-4">
                         <button onClick={publishBlog} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Publish your Blog</button>       
                     </div>
-
-                    {title}
-                    {content}
 
                 </div>
             </div>
